308f19a811eeaa15aab2da6217309fc42bbf4846
uNdimka <undimka@gmail.com>
2017-05-08 17:03:30 +0300

    try it panel update;

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -14,13 +14,14 @@ requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
             tryit: function(this_e){
                 $tryit = this_e.extSetHtmlTryIt(this_e.getTemplate('tryit'));
 
-                var tryitDataInput = $tryit.find('.tryit-content');
-                tryitDataInput.focus();
+                var tryitTextInput = $tryit.find('.tryit_text_input');
+                var tryitWordsInput = $tryit.find('.tryit_words_input');
+                tryitTextInput.focus();
 
                 $tryit.find('.bn-check').click(function (e) {
-                    var password = passwordInput.val();
-                    var tryitData = tryitDataInput.val();
-                    this_e.extSendToConsoleCheckiO(tryitData);
+                    var tryitTextData = tryitTextInput.val();
+                    var tryitWordsData = tryitWordsInput.val().split(/[\s,]+/);
+                    this_e.extSendToConsoleCheckiO(tryitTextData, tryitWordsData);
                     e.stopPropagation();
                     return false;
                 });
